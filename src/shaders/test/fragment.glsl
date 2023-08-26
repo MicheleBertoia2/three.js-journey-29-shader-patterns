e35079a4496ff6b9108b47ad94482f1cd7955335
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-08-26 19:15:29 +0200

    [more] patterns

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -15,7 +15,52 @@ void main()
     // float strength = vUv.y * 10.0;
 
     //Pattern 7
-    // float strength = vUv.y * 10.0;
+    // float strength = mod(vUv.y * 10.0, 1.0);
+
+    //Pattern 8
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    
+    // strength = step(0.5, strength);
+
+    //Pattern 9
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    
+    // strength = step(0.8, strength);
+
+    //Pattern 10
+    // float strength = mod(vUv.x * 10.0, 1.0);
+    
+    // strength = step(0.8, strength);
+
+    //Pattern 11
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0)); 
+    // strength +=  step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    //Pattern 12
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0)); 
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    //Pattern 13
+    // float strength = step(0.3, mod(vUv.x * 10.0, 1.0)); 
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    //Pattern 14
+    // float barX = step(0.3, mod(vUv.x * 10.0, 1.0)); 
+    // barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    
+    // float barY = step(0.8, mod(vUv.x * 10.0, 1.0)); 
+    // barY *= step(0.3, mod(vUv.y * 10.0, 1.0));
+
+    // float strength = barX + barY;
+
+    //Pattern 15
+    float barX = step(0.3, mod(vUv.x * 10.0, 1.0)); 
+    barX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+    
+    float barY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0)); 
+    barY *= step(0.3, mod(vUv.y * 10.0, 1.0));
+
+    float strength = barX + barY;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
