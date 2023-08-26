7a7e0050663b06fd9d0d16a1bbbca4a343074724
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-08-26 18:59:08 +0200

    [drawing some shaders]

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -2,5 +2,20 @@ varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    //Pattern 3
+    // float strenght = vUv.x;
+
+    //Pattern 4
+    // float strength = vUv.y;
+
+    //Pattern 5
+    // float strength = 1.0 - vUv.y;
+
+    //Pattern 6
+    // float strength = vUv.y * 10.0;
+
+    //Pattern 7
+    // float strength = vUv.y * 10.0;
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
